0d28cabfeaf7ffb46cf3e9507b98d069188fdf70
Zé Marcos <contato.marcosrf@gmail.com>
2021-12-20 22:57:26 -0300

    Many to Many

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,11 @@ Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create'])->middleware('auth');
 Route::get('/events/{id}', [EventController::class, 'show']);
 Route::post('/events', [EventController::class, 'store']);
+Route::delete('/events/{id}', [EventController::class, 'destroy'])->middleware('auth');
+Route::get('/events/edit/{id}', [EventController::class, 'edit'])->middleware('auth');
+Route::put('events/update/{id}', [EventController::class, 'update'])->middleware('auth');
 
-Route::get('/contact', [ContactController::class, 'contact']);
+Route::get('/contact', [ContactController::class, 'contact'])->middleware('auth');
 
 
 
@@ -18,4 +21,7 @@ Route::get('/dashboard',[EventController::class, 'dashboard'])->middleware('auth
 
 
 Route::get('/products', [ProductController::class, 'product']);
+
+
+Route::post('events/join/{id}',[EventController::class, 'joinEvent'])->middleware('auth');
 
